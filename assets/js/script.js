2bba3c2fb37934422f99a98b90bb7864b0b80188
Alex Widener <awidener3@gmail.com>
2022-01-10 15:24:00 -0500

    searched term now appends too searched buttons, and the last button is removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,7 @@ renderForecast = (openWeatherData) => {
 // };
 
 renderSearchHistory = () => {
+    searchedButtons.textContent = '';
 
     for (let i = 0; i < searchHistory.length; i++) {
         let button = document.createElement('button');
@@ -106,8 +107,10 @@ renderSearchHistory = () => {
     }
 };
 
-appendToSearchHistory = () => {
-
+// TODO: Fix text remaining undercase and format for better UI
+appendToSearchHistory = (city) => {
+    searchHistory.unshift(city);
+    searchHistory.pop();
 };
 
 
@@ -115,6 +118,8 @@ appendToSearchHistory = () => {
 searchButton.addEventListener('click', function (event) {
     event.preventDefault();
     cityName = searchInput.value.toLowerCase().trim();
+    appendToSearchHistory(cityName);
+    renderSearchHistory();
     fetchWeather();
     searchInput.value = '';
 });
